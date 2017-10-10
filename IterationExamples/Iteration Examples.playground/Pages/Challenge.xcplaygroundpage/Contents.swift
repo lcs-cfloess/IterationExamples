476677d90007c431686b09039bace4edaee08384
Floess, Constantin <cfloess@lcs-mba3343.lcs.on.ca>
2017-10-10 15:04:26 -0400

    crazy thing

--- IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
+++ IterationExamples/Iteration Examples.playground/Pages/Challenge.xcplaygroundpage/Contents.swift
@@ -16,10 +16,26 @@ let canvas = Canvas(width: 300, height: 300)
 
 // Below this line, try combining a loop and four statements that draw lines to generate the goal
 for i in stride(from: 0, through: 300, by: 20){
+    canvas.lineColor = Color.blue
     canvas.drawLine(fromX: i, fromY: 0, toX: 300, toY: i)
     canvas.drawLine(fromX: i, fromY: 300, toX: 0, toY: i)
+    canvas.lineColor = Color.orange
     canvas.drawLine(fromX: i, fromY: 0, toX: 0, toY: 300-i)
     canvas.drawLine(fromX: i, fromY: 300, toX: 300, toY: 300-i)
+    
+    canvas.lineColor = Color.green
+    canvas.drawLine(fromX: i, fromY: 50, toX: 250, toY: i)
+    canvas.drawLine(fromX: i, fromY: 250, toX: 50, toY: i)
+    canvas.lineColor = Color.yellow
+    canvas.drawLine(fromX: i, fromY: 50, toX: 50, toY: 300-i)
+    canvas.drawLine(fromX: i, fromY: 250, toX: 250, toY: 300-i)
+    
+    canvas.lineColor = Color.purple
+    canvas.drawLine(fromX: i, fromY: 100, toX: 200, toY: i)
+    canvas.drawLine(fromX: i, fromY: 200, toX: 100, toY: i)
+    canvas.lineColor = Color.red
+    canvas.drawLine(fromX: i, fromY: 100, toX: 100, toY: 300-i)
+    canvas.drawLine(fromX: i, fromY: 200, toX: 200, toY: 300-i)
 }
 
 
